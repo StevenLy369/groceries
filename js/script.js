d8446ebc8e93242d8bfd041b0f2e9c793bea62f7
Steven Ly <1stevenly92@gmail.com>
2020-03-30 11:55:08 -0700

    added inputs and stored in array

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+    $("#list").submit(function(event) {
+
+        event.preventDefault();
+
+
+
+        var listArray = [($("input#one").val()) , ($("input#two").val()) , ($("input#two").val()) , ($("input#two").val())   ]
+
+        console.log(listArray);
+
+
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
